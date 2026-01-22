43b95b5be85d85b6182929c9b196a3cdd7e4ec4d
Kwyjibo501 <c28logan.goebel@afacademy.af.edu>
2026-01-22 11:18:57 -0700

    ICE2 halfadder

--- src/halfAdder.vhd
+++ src/halfAdder.vhd
@@ -10,7 +10,12 @@
 --| 
 --| ---------------------------------------------------------------------------
 --|
+--| FILENAME      : halfAdder.vhd
+--| AUTHOR(S)     : C3C Goebel
+--| CREATED       : 01/22/2026
 --| DESCRIPTION   : This file implements a one bit half adder.
+--|
+--| DOCUMENTATION : None
 --|
 --+----------------------------------------------------------------------------
 --|
@@ -29,8 +34,9 @@ entity halfAdder is
   port(
 	i_A     : in  std_logic; -- 1-bit input port
 	i_B     : in  std_logic; 
-	o_S     : out std_logic  -- 1-bit output port (NOTE: NO semicolon on LAST port only!)	    
+	o_S     : out std_logic;  -- 1-bit output port (NOTE: NO semicolon on LAST port only!)	    
 	-- TODO:  Carry port
+	o_Cout     : out std_logic
   ); -- the semicolon is here instead
 end halfAdder;
 
@@ -43,5 +49,6 @@ begin
 	-- *concurrent* signal assignments
 	o_S    <= i_A xor i_B;
 	-- TODO:  Carry signal assignment
+	o_Cout <= i_A and i_B;
 	
 end halfAdder_arch;
